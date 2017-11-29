503f789530c427f087c56d30a6d1b54d8f8e5599
胡劼 <xsir317@gmail.com>
2017-11-30 00:23:22 +0800

    一些文档

--- web/environments/prod/common/config/main-local.php
+++ web/environments/prod/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=renju',
             'username' => 'root',
-            'password' => 'Fz1$4fB3jf',
+            'password' => '',
             'charset' => 'utf8',
         ],
         'redis' => [
