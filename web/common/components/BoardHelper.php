8ba102eec2d75365e56eed342180d2bc8ccffea0
xsir317 <xsir317@gmail.com>
2019-01-18 17:26:03 +0800

    好像完工了，测测看

--- web/common/components/BoardHelper.php
+++ web/common/components/BoardHelper.php
@@ -64,18 +64,18 @@ class BoardHelper
     private $current = [1,1];
 
     /**
-     * @param null $pos
+     * @param null $shape
      * @return array
      *
      * 8个方向，切割一下，两两返回，分别是 | -  \ / 四个方向
      */
-    private function get_positions($pos = null)
+    private function get_positions($shape = null)
     {
         $pos_shapes = self::$pos_shapes;
         $chunk =  array_chunk(self::$directions,2);
-        if($pos && isset($pos_shapes[$pos]))
+        if($shape && isset($pos_shapes[$shape]))
         {
-            return $chunk[ $pos_shapes[$pos] ];
+            return $chunk[ $pos_shapes[$shape] ];
         }
         return $chunk;
     }
@@ -223,50 +223,155 @@ class BoardHelper
 
     /**
      * @param $coordinate
-     * @param $color
      * @param string $shape
-     * @return bool
-     * 判断指定坐标落子【在指定方向上】是否形成了四；
+     * @return int
+     * 判断指定坐标落子【在指定方向上】是否形成了四； 返回可能是0 1 2
      */
-    private function isFour($coordinate, $color, $shape = '')
+    private function isFour($coordinate, $shape = '|')
     {
         if($this->_($coordinate) != self::EMPTY_STONE)
         {
             return false;
         }
-
+        $result = 0;
+        //放棋子
+        $this->setStone(self::BLACK_STONE,$coordinate);
+        //沿着每个方向去找空格，顺便数黑棋
+        $count_black = 1;//刚刚手动放了一个的
+        foreach (self::get_positions($shape) as $direction)
+        {
+            $this->moveTo($coordinate);
+            while(self::BLACK_STONE == $this->moveDirection($direction))
+            {
+                $count_black ++;
+            }
+            if($this->_() == self::EMPTY_STONE)
+            {
+                //对此空格判断是否能连5
+                if($this->isFive($this->current,self::BLACK_STONE,$shape))
+                {
+                    $result ++;
+                }
+            }
+        }
+        //如果两边都能连5，则可能有一个特殊情况
+        if($count_black == 4 && $result == 2)
+        {
+            $result = 1;
+        }
+        //恢复空格
+        $this->setStone(self::EMPTY_STONE,$coordinate);
+        return $result;
     }
 
     /**
      * @param $coordinate
-     * @param $color
      * @param string $shape
+     * @return bool
      * 判断指定坐标落子【在指定方向上】是否形成了活四；
      * 对于假禁手问题的针对性判断：  如果此落点形成了禁手，则直接返回false；
      * 所以判断 IsFour 的时候，会返回真； 而IsOpenFour 则返回false  因为落点是禁手点，不认为其具有攻击性。
      * 此点为禁手，则此点相关的四 是四，但不算活四。 与之相关的三 一定不是活三
      */
-    private function isOpenFour($coordinate,$color,$shape = '')
+    private function isOpenFour($coordinate,$shape = '|')
     {
-
+        if($this->_($coordinate) != self::EMPTY_STONE)
+        {
+            return false;
+        }
+        if($this->isForbidden($coordinate))
+        {
+            return false;
+        }
+        $count_active = 0;
+        //放棋子
+        $count_black = 1;//当前点肯定是黑棋
+        $this->setStone(self::BLACK_STONE,$coordinate);
+        foreach (self::get_positions($shape) as $direction)
+        {
+            $this->moveTo($coordinate);
+            //沿着每个方向去找空格，顺便数黑棋
+            while(self::BLACK_STONE == $this->moveDirection($direction))
+            {
+                $count_black ++;
+            }
+            if($this->_() == self::EMPTY_STONE)
+            {
+                //对此空格判断是否能连5
+                if($this->isFive($this->current,self::BLACK_STONE,$shape))
+                {
+                    $count_active ++;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        //恢复空格
+        $this->setStone(self::EMPTY_STONE,$coordinate);
+        if($count_black == 4 && $count_active == 2)
+        {
+            return true;
+        }
+        return false;
     }
 
     /**
      * @param $coordinate
-     * @param $color
      * @param string $shape
+     * @return bool
      */
-    private function isOpenThree($coordinate,$color,$shape = '')
+    private function isOpenThree($coordinate,$shape = '|')
     {
-
+        $return = false;
+        $this->setStone(self::BLACK_STONE,$coordinate);
+        //沿着每个方向去找空格
+        foreach (self::get_positions($shape) as $direction)
+        {
+            $this->moveTo($coordinate);
+            while(self::BLACK_STONE == $this->moveDirection($direction))
+            {
+                null;//一直挪，这个方向走到不是黑棋的地方
+            }
+            if($this->_() == self::EMPTY_STONE)
+            {
+                //对此空格判断是否能活四
+                if($this->isOpenFour($this->current,$shape))
+                {
+                    $return = true;
+                    break;//能活四的话另一边不用看了，不能活四再看另一头
+                }
+            }
+            else//如果落子的地方有一头不是空格，那看也不用看了。。。
+            {
+                break;
+            }
+        }
+        //恢复空格
+        $this->setStone(self::EMPTY_STONE,$coordinate);
+        return $return;
     }
 
     /**
      * @param $coordinate
+     * @return bool
      */
     private function isDoubleThree($coordinate)
     {
-
+        $count = 0;
+        foreach (self::$pos_shapes as $s => $i)
+        {
+            if($this->isOpenThree($coordinate,$s))
+            {
+                $count ++;
+                if($count >= 2)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
@@ -277,7 +382,7 @@ class BoardHelper
     {
         $count = 0;
         foreach (self::$pos_shapes as $s => $i) {
-            $count += $this->isFour($coordinate,self::BLACK_STONE,$s);
+            $count += $this->isFour($coordinate,$s);
             if($count >= 2)
             {
                 return true;
@@ -294,14 +399,6 @@ class BoardHelper
      */
     private function isOverline($coordinate)
     {
-        if($this->_($coordinate) != self::EMPTY_STONE)
-        {
-            return false;
-        }
-        if($this->isFive($coordinate,self::BLACK_STONE))
-        {
-            return false;
-        }
         $this->setStone(self::BLACK_STONE,$coordinate);
         $result = false;
         $count = 0;
@@ -350,12 +447,25 @@ class BoardHelper
             {
                 return self::BLACK_FIVE;
             }
-            if($this->isOverline($coordinate) || $this->isDoubleFour($coordinate) || $this->isDoubleThree($coordinate))
+            if($this->isForbidden($coordinate))
             {
                 return self::BLACK_FORBIDDEN;
             }
         }
         return false;
+    }
+
+    public function isForbidden($coordinate)
+    {
+        if($this->_($coordinate) != self::EMPTY_STONE)
+        {
+            return false;
+        }
+        if($this->isFive($coordinate,self::BLACK_STONE))
+        {
+            return false;
+        }
+        return ($this->isOverline($coordinate) || $this->isDoubleFour($coordinate) || $this->isDoubleThree($coordinate));
     }
 
     /**
