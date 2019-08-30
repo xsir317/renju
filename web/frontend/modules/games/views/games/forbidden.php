4a35cb2cdd38547aa73edcea4164e0c0fb867398
xsir317 <xsir317@gmail.com>
2019-08-30 11:24:57 +0800

    private检查

---
+++ web/frontend/modules/games/views/games/forbidden.php
@@ -0,0 +1,22 @@
+<?php
+
+$this->title = Yii::t('app','Private Game');
+
+?>
+<div class="layui-main site-inline" style="margin-top: 20px;overflow: hidden;">
+    <div class="wrapper" style="overflow:hidden">
+        <div class="site-error">
+
+            <h1>Forbidden (#403)</h1>
+
+            <div class="alert alert-danger">
+            </div>
+
+            <p>
+                这是一个私密对局，您不能查看。
+            </p>
+            <p>
+                This is a private game, YOU shall not pass!
+            </p>
+        </div>
+    </div></div>
