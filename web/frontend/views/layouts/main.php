cbde675291c58903d821cc76c4cd439cd889d4d0
胡劼 <xsir317@gmail.com>
2022-10-02 23:21:29 +0800

    jquery cdn fix

--- web/frontend/views/layouts/main.php
+++ web/frontend/views/layouts/main.php
@@ -135,11 +135,11 @@ const rule_defines = (<?php echo json_encode(CommonService::getRules()) ?>);
 
 const ts_delta = (<?php echo time() ?> - Math.round(new Date().getTime()/1000));
 </script>
-<script src="//libs.baidu.com/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
+<script src="//cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript">
-    if (typeof jQuery == 'undefined') {
-        document.write(unescape("%3Cscript src='http://lib.sinaapp.com/js/jquery/1.12.4/jquery-1.12.4.min.js' type='text/javascript'%3E%3C/script%3E"));
-    }
+if (typeof jQuery == 'undefined') {
+    document.write(unescape("%3Cscript src='http://lib.sinaapp.com/js/jquery/1.12.4/jquery-1.12.4.min.js' type='text/javascript'%3E%3C/script%3E"));
+}
 </script>
 <script src="/js/all.js?v=21" type="text/javascript" charset="utf-8"></script>
 <?php $this->endBody() ?>
